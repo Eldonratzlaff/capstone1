9e57c127e361fba05504195e77804cc4212aa6e1
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-01-13 19:52:26 -0300

    changes tho js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+
+
 const moreBtn = document.querySelector('.botom-btn');
 
 const speaker = document.getElementById('speaker-ul2');
 let speakerTogle = 0;
 
-
 function loadEmpty() {
-    speaker.innerHTML = ``;
+  speaker.innerHTML = "";
 }
 function loadPages() {
-    speaker.innerHTML =
-   ` <li class="speakers-li">
+  speaker.innerHTML = ` <li class="speakers-li">
             <div class="image speaker3"></div>
             <div class="speaker-content">
                 <h5 class="speaker-name">SohYeong Noh</h5>
@@ -29,17 +29,17 @@ function loadPages() {
 }
 
 moreBtn.addEventListener('click', () => {
-    if (speakerTogle === 0) {
-      speakerTogle = 1;
-      loadPages();} 
+  if (speakerTogle === 0) {
+    speakerTogle = 1;
+    loadPages();} 
     else {loadEmpty();
        speakerTogle = 0;
-      }
-  });
+  }
+});
 const hamburgerPage = document.querySelector('.hamburger-page');
 const hamburgeBtn = document.querySelector('.hamburger');
 hamburgeBtn.addEventListener('click', () => {
-  hamburgerPage.classList.add('display');})
+  hamburgerPage.classList.add('display'); })
 const xBtn = document.querySelector('.x-btn');
 xBtn.addEventListener('click', () =>{
      hamburgerPage.classList.remove('display');
