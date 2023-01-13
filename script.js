faed31f856e28e1037789539dfebffb4c9403fa3
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-01-13 19:39:36 -0300

    mod js file

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const moreBtn = document.querySelector('.botom-btn');
 
 const speaker = document.getElementById('speaker-ul2');
-var speakerTogle = 0;
+let speakerTogle = 0;
 moreBtn.addEventListener('click', () => {
-if (speakerTogle == 0){
-    speakerTogle = 1
-    loadPages();
-} else {loadEmpty();
-speakerTogle = 0;
-}
+  if (speakerTogle == 0) {
+    speakerTogle = 1;
+    loadPages();} 
+  else {loadEmpty();
+     speakerTogle = 0;
+    }
 });
 
-function loadEmpty(){
-    speaker.innerHTML = ``;
+function loadEmpty() {
+     speaker.innerHTML = ``;
 }
 
-function loadPages(){
-speaker.innerHTML =
+function loadPages() {
+    speaker.innerHTML =
    ` <li class="speakers-li">
             <div class="image speaker3"></div>
             <div class="speaker-content">
@@ -33,14 +33,13 @@ speaker.innerHTML =
                 <p>European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July</p>
                 <p></p>
             </div>
-        </li> `
+        </li> `;
 }
 const hamburgerPage = document.querySelector('.hamburger-page');
 const hamburgeBtn = document.querySelector('.hamburger');
-hamburgeBtn.addEventListener('click', () =>{
-hamburgerPage.classList.add('display');
-} )
+hamburgeBtn.addEventListener('click', () => {
+  hamburgerPage.classList.add('display');})
 const xBtn = document.querySelector('.x-btn');
 xBtn.addEventListener('click', () =>{
-    hamburgerPage.classList.remove('display');
+     hamburgerPage.classList.remove('display');
 })
